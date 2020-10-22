b9cf72c698308a66a0e63700794a85c5c4b726c8
Chukwuemeka Okoli <32660991+MachinatorX@users.noreply.github.com>
2020-10-22 06:26:20 -0800

    Create ProgrammingforEverybody folder

---
+++ 01_ProgrammingforEverybody/Assignments/ass.py
@@ -0,0 +1,6 @@
+# Program to compute gross pay by accepting data from user
+
+hrs = float(input("Enter Hours:"))
+rate = float(input("Enter Rate per hour:"))
+gross_pay = rate * hrs
+print("Pay:", gross_pay)
