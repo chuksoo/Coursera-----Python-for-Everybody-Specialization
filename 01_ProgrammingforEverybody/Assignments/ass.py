a721f65c3c560705fd0e4af36d00cfd203804181
Chukwuemeka Okoli <32660991+MachinatorX@users.noreply.github.com>
2020-10-22 11:31:38 -0800

    Delete ass.py

--- 01_ProgrammingforEverybody/Assignments/ass.py
+++
@@ -1,6 +0,0 @@
-# Program to compute gross pay by accepting data from user
-
-hrs = float(input("Enter Hours:"))
-rate = float(input("Enter Rate per hour:"))
-gross_pay = rate * hrs
-print("Pay:", gross_pay)
